9f09152d02191ece5f7429ef13667ff9d8a769e4
Trishgachau <beatricegachau@gmail.com>
2024-07-10 21:58:05 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React from "react";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://https://github.com/Trishgachau" target="_blank">
-          Beatrice Gachau{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://https://github.com/Trishgachau/react-weather-app"
-          target="_blank"
-        >
-          open sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a href="https://https://github.com/Trishgachau" target="_blank">
+            Beatrice Gachau{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://https://github.com/Trishgachau/react-weather-app"
+            target="_blank"
+          >
+            open sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
