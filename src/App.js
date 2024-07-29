129e04ed9d8b38b111ad72874aaf30d0fdb48028
Trishgachau <beatricegachau@gmail.com>
2024-07-29 20:33:51 +0300

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -24,6 +24,14 @@ export default function App() {
             rel="noreferrer"
           >
             open sourced on Github
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-weather-app98.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
         </footer>
       </div>
